125f3122a899e6f5ceeba7ca838594acbd4130dd
Ron Rice <ron31416@gmail.com>
2025-09-12 10:49:00 -0600

    modify layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,16 @@
+// src/app/layout.tsx
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Music Portal",
   description: "",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" }, // default, legacy
+      { url: "/favicon.png", type: "image/png", sizes: "32x32" }, // modern sharp
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -24,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
