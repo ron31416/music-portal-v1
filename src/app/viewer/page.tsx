bafecb712f83a8453110ed3cbbe80d48659e8971
Ron Rice <ron31416@gmail.com>
2025-10-09 08:44:09 -0600

    main page lookup 7

--- src/app/viewer/page.tsx
+++ src/app/viewer/page.tsx
@@ -1,5 +1,5 @@
 // src/app/viewer/page.tsx
-import RedirectToSandbox from "../../components/RedirectToSandbox";
+
 import ViewerClient from "./viewer-client";
 
 export const dynamic = "force-dynamic";
@@ -7,8 +7,6 @@ export const dynamic = "force-dynamic";
 export default function ViewerPage() {
   return (
     <main className="p-4">
-      {/* Mount the redirect first so it runs immediately */}
-      <RedirectToSandbox />
       <ViewerClient />
     </main>
   );
