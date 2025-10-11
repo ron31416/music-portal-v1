eb9c82692a267bea0b5afa2763df8acc36b37e79
Ron Rice <ron31416@gmail.com>
2025-10-11 08:56:26 -0600

    cleanup 3

--- src/types/opensheetmusicdisplay-augment.d.ts
+++
@@ -1,9 +0,0 @@
-// Augment OSMD so TS knows about the Zoom property that exists at runtime.
-import "opensheetmusicdisplay";
-
-declare module "opensheetmusicdisplay" {
-  interface OpenSheetMusicDisplay {
-    /** 1 = 100% scale. Undocumented in types; present on the instance at runtime. */
-    Zoom?: number;
-  }
-}
